3d76b652c4d42a5c8776d4dd0782ef83ce85ee02
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-01-10 03:29:12 +0800

    added svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+
+<main>
+	<h1>WElcome {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<script>
+	export let name;
+</script>
+
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: lightgreen;
+		text-transform: lowercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
