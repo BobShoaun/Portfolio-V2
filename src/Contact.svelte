cb4596236eb7336d36cfbff12e3caa4c9e0c9e02
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-02-06 08:04:00 +0800

    netlify forms

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -3,14 +3,24 @@
 	<p class="text-gray-500 dark:text-gray-300 text-xl mb-10">
 		What are you waiting for? Shoot me a message and I will get back to you as soon as I can. <br />
 		My email is <a href="mailto:ngbobshoaun2000@gmail.com">ngbobshoaun2000@gmail.com</a>.
-    </p>
-	<input class="w-full mb-2 px-4 py-2 dark:bg-gray-600" type="text" placeholder="Your email" />
-	<textarea
-		class="w-full px-4 py-2 mb-3 min-h-1/4 dark:bg-gray-600"
-		placeholder="Hey..."
-		name="fsdfs"
-		id=""
-		rows="10"
-	/>
-	<button class="bg-green-200 py-1 px-4 float-right rounded-none font-bold font-mono"><i class="far fa-paper-plane mr-3" />Send</button>
+	</p>
+
+	<form action="POST" data-netlify="true">
+		<input
+			name="email"
+			class="w-full mb-2 px-4 py-2 dark:bg-gray-600 dark:text-gray-50"
+			type="text"
+			placeholder="Your email"
+		/>
+		<textarea
+			name="message"
+			class="w-full px-4 py-2 mb-3 min-h-1/4 dark:bg-gray-600 dark:text-gray-50"
+			placeholder="Hey..."
+			id=""
+			rows="10"
+		/>
+    <div data-netlify-recaptcha="true"></div>
+		<button class="bg-green-200 py-1 px-4 float-right rounded-none font-bold font-mono"
+			><i class="far fa-paper-plane mr-3" />Send</button>
+	</form>
 </main>
