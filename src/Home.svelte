09aadf9ac6510ca0bb46ef75ec1204ed515230f2
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-02-22 21:04:35 +0800

    added cool animated box

--- src/Home.svelte
+++ src/Home.svelte
@@ -13,48 +13,63 @@
 	$: welcomeMessage = dark ? "Hey there." : "Hello there.";
 </script>
 
-<main id="home" class="relative main bg-gray-50 dark:bg-gray-700 h-screen flex">
+<main
+	id="home"
+	class="relative main bg-gray-50 -dark:bg-gray-700 h-screen flex bg-gradient-to-b dark:from-gray-700 dark:to-gray-900"
+>
 	<!-- <div class="background absolute bg-gray-100" /> -->
 
 	<!-- <section class="min-h-screen flex"> -->
-	<section class="section w-full">
-		<div class="bg-purple-400 w-7 h-1.5 lg:w-12 lg:h-3 ml-0.5 mb-2" />
-		<!-- {#if ready} -->
-		<h1
-			class="animate__animated animate__fadeIn text-5xl md:text-6xl lg:text-7xl font-extrabold text-gray-600 dark:text-white mb-2"
-		>
-			{welcomeMessage}
-		</h1>
-		<p
-			class="animate__animated animate__fadeInDown text-lg md:text-xl lg:text-2xl mb-10 font-bold text-gray-500 dark:text-gray-400 ml-1"
-		>
-			I am Ng Bob Shoaun. I write good code with good designs.
-		</p>
-		<!-- {/if} -->
-		<!-- <a
-				type="button"
-				href="/images/resume.pdf"
-				target="_blank"
-				class="ml-1 py-2 px-4 border-green-400 border bg-green-200 hover:bg-green-300 rounded-none mr-3">
-				<p class="text-gray-700 font-mono">View resume</p>
-      </a> -->
-		<div class="animate__animated animate__fadeInDown animate__delay-0.5s">
-			<Link
-				to="resume"
-				class="px-4 py-2 mb-3 lg:ml-1 inline-block leading-4 border-green-400 border bg-green-200 hover:bg-green-300 rounded-none mr-3 text-gray-700 font-mono"
-				noroute
+	<section class="section w-full flex flex-col-reverse justify-between lg:flex-row">
+		<div class="hero">
+			<div class="bg-purple-400 w-7 h-1.5 lg:w-12 lg:h-3 ml-0.5 mb-2" />
+			<!-- {#if ready} -->
+			<h1
+				class="animate__animated animate__fadeIn text-6xl md:text-7xl lg:text-7xl font-extrabold text-gray-600 dark:text-white mb-2"
 			>
-				<p class="text-gray-700 font-semibold font-mono">View resume</p></Link
+				{welcomeMessage}
+			</h1>
+			<p
+				class="animate__animated animate__fadeInDown text-lg md:text-xl lg:text-2xl mb-10 font-bold text-gray-500 dark:text-gray-400 ml-1"
 			>
-			<a
-				type="button"
-				href="#contact"
-				class="px-4 py-2 inline-block border-blue-400 border bg-blue-200 hover:bg-blue-300 rounded-none"
-				><p class="text-gray-700 font-semibold font-mono leading-4">Let's talk</p></a
-			>
+				I am Ng Bob Shoaun. I write good code with good designs.
+			</p>
+			<!-- {/if} -->
+			<!-- <a
+          type="button"
+          href="/images/resume.pdf"
+          target="_blank"
+          class="ml-1 py-2 px-4 border-green-400 border bg-green-200 hover:bg-green-300 rounded-none mr-3">
+          <p class="text-gray-700 font-mono">View resume</p>
+        </a> -->
+			<div class="animate__animated animate__fadeInDown animate__delay-0.5s">
+				<Link
+					to="resume"
+					class="px-4 py-2 mb-3 lg:ml-1 inline-block leading-4 border-green-400 border bg-green-200 hover:bg-green-300 rounded-none mr-3 text-gray-700 font-mono"
+					noroute
+				>
+					<p class="text-gray-700 font-semibold font-mono">View resume</p></Link
+				>
+				<a
+					type="button"
+					href="#contact"
+					class="px-4 py-2 inline-block border-blue-400 border bg-blue-200 hover:bg-blue-300 rounded-none"
+					><p class="text-gray-700 font-semibold font-mono leading-4">Let's talk</p></a
+				>
+			</div>
 		</div>
+		<div
+			class="bg-gradient-to-b via-green-300 from-indigo-400 to-blue-300 -border-gray-300 -border-8 -border-dotted w-full h-20 mb-10 lg:w-52 lg:h-52 design-element"
+		/>
 	</section>
 
+	<!-- <div class="absolute w-40 h-40 bg-transparent border-gray-300 border-8 top-64 right-64 design-element"></div> -->
+	<p
+		data-aos="fade-down"
+		class="text-xl lg:text-3xl text-gray-700 dark:text-white text-center absolute bottom-28 right-0 left-0"
+	>
+		<i class="fas fa-angle-down" />
+	</p>
 </main>
 
 <style>
@@ -66,4 +81,23 @@
 		top: -500px;
 		z-index: -1;
 	}
+	:root {
+		--gradient: linear-gradient(45deg, red, green
+    );
+	}
+	.design-element {
+		/* border-radius: 5px; */
+		background-size: 100% 200%;
+    background-position: top;
+    animation: bg-animation 10s infinite alternate;
+	}
+/* 
+  .design-element:hover, .design-element:focus {
+    background-position: right;
+  } */
+
+  @keyframes bg-animation {
+    0% { background-position: top; }
+    100% { background-position: bottom; }
+  }
 </style>
