a6099ba3a528927948ad02e60ddba5ef4fb76294
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-03-08 02:21:51 +0800

    adjusted hero anims

--- src/Hero.svelte
+++ src/Hero.svelte
@@ -19,7 +19,7 @@
 >
 	<div
 		data-aos="slide-down"
-		data-aos-delay="900"
+		data-aos-delay="700"
 		class="absolute background top-0 right-0 h-1/2 w-full md:w-1/3 md:h-full moving-gradient-1"
 	/>
 
@@ -30,20 +30,20 @@
 		<div class="bg-white dark:bg-gray-900 px-5 py-10 lg:py-20 lg:px-10 shadow-2xl">
 			<div
 				data-aos="flip-up"
-				data-aos-delay="700"
+				data-aos-delay="600"
 				class="bg-purple-400 w-8 h-1.5 lg:w-10 lg:h-2 ml-0.5 mb-2"
 			/>
 			<!-- {#if ready} -->
 			<h1
 				data-aos="zoom-in-right"
-				data-aos-delay="900"
+				data-aos-delay="800"
 				class="font-mono text-2xl md:text-4xl lg:text-5xl font-extrabold text-gray-600 dark:text-white mb-5"
 			>
 				{welcomeMessage}
 			</h1>
 			<p
 				data-aos="zoom-in-right"
-				data-aos-delay="1100"
+				data-aos-delay="1000"
 				class="font-mono text-lg md:text-xl lg:text-2xl mb-10 font-bold text-gray-500 dark:text-gray-400 ml-1"
 			>
 				// I'm Ng Bob Shoaun. I write good code with good designs.
@@ -56,11 +56,7 @@
           class="ml-1 py-2 px-4 border-green-400 border bg-green-200 hover:bg-green-300 rounded-none mr-3">
           <p class="text-gray-700 font-mono">View resume</p>
         </a> -->
-			<div
-				data-aos="flip-up"
-				data-aos-delay="1300"
-				class=""
-			>
+			<div data-aos="flip-up" data-aos-delay="1200" class="">
 				<Link
 					to="resume"
 					class="px-4 py-2 lg:ml-1 inline-block leading-4 bg-green-200 border rounded-sm border-green-400  hover:bg-green-300 mr-3 text-gray-700 font-mono"
@@ -78,8 +74,11 @@
 		</div>
 	</section>
 
-	<div data-aos="slide-up"
-  data-aos-delay="2000" class="hidden lg:block absolute left-14 bottom-0 text-center">
+	<div
+		data-aos="slide-up"
+		data-aos-delay="1500"
+		class="hidden lg:block absolute left-14 bottom-0 text-center"
+	>
 		<a href="https://github.com/BobShoaun" target="_blank" class="block mb-8"
 			><i class="text-gray-800 dark:text-gray-50 fab fa-github fa-lg" /></a
 		>
@@ -97,7 +96,7 @@
 	<!-- <div class="absolute w-40 h-40 bg-transparent border-gray-300 border-8 top-64 right-64 design-element"></div> -->
 	<p
 		data-aos="fade-down"
-		data-aos-delay="2000"
+		data-aos-delay="1500"
 		on:click={() => document.getElementById("about").scrollIntoView()}
 		class="text-xl lg:text-3xl text-gray-700 dark:text-white cursor-pointer text-center absolute bottom-28 right-0 left-0"
 	>
@@ -114,7 +113,7 @@
 		/* top: -500px; */
 		/* z-index: -1; */
 	}
-	
+
 	.design-element {
 		/* border-radius: 5px; */
 
@@ -122,12 +121,9 @@
 		background-position: top;
 		animation: gradient-animation-y 10s infinite alternate;
 	}
-	
 
 	/* 
   .design-element:hover, .design-element:focus {
     background-position: right;
   } */
-
-
 </style>
