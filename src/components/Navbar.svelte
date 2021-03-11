31e0d07c9f6b92f37f0f35bbb06107992c3e19b3
Ng Bob Shoaun <ngbobshoaun2000@gmail.com>
2021-03-11 20:57:26 +0800

    navbar hover and fix

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -30,15 +30,24 @@
 			// navbar.style.top = prevScrollpos > currentScrollPos ? "0" : `-${navbar.offsetHeight}px`;
 			prevScrollpos = currentScrollPos;
 		};
+
+    // show navbar when hover over
+		onmousemove = e => {
+			if (e.clientY < navbar.offsetHeight) navbar.style.top = "0";
+		};
 	});
 </script>
 
 <header
 	id="navbar"
-	class="flex items-center justify-between px-6 lg:px-10 py-5 bg-white dark:bg-gray-900 shadow-md fixed w-full z-50"
+	class="flex items-center justify-between px-6 lg:px-10 py-4 top-0 bg-white dark:bg-gray-900 shadow-md fixed w-full z-50"
 >
 	<div class="logo font-semibold">
-		<div data-aos="flip-up" data-aos-duration="700" class="bg-purple-400 w-4 h-4 mr-3 inline-block" />
+		<div
+			data-aos="flip-up"
+			data-aos-duration="700"
+			class="bg-purple-400 w-4 h-4 mr-3 inline-block"
+		/>
 		<a href="/#home" data-aos="flip-up" class="hover:no-underline inline-block"
 			><p class="text-gray-800 dark:text-gray-50">Ng Bob Shoaun</p></a
 		>
@@ -48,12 +57,20 @@
 			<a href="/#about" data-aos="zoom-in" data-aos-delay="100" data-aos-duration="700" noroute
 				><p class="nav-text">About</p></a
 			>
-			<a href="/#web-development" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="700" noroute
-				><p class="nav-text">Web dev</p></a
+			<a
+				href="/#web-development"
+				data-aos="zoom-in"
+				data-aos-delay="200"
+				data-aos-duration="700"
+				noroute><p class="nav-text">Web dev</p></a
 			>
 			<!-- <a href="/#graphic-design" noroute><p class="text-gray-800 dark:text-gray-50 font-mono ">Design</p></a> -->
-			<a href="/#game-development" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="700" noroute
-				><p class="nav-text">Game dev</p></a
+			<a
+				href="/#game-development"
+				data-aos="zoom-in"
+				data-aos-delay="300"
+				data-aos-duration="700"
+				noroute><p class="nav-text">Game dev</p></a
 			>
 			<a href="/#contact" data-aos="zoom-in" data-aos-delay="400" data-aos-duration="700" noroute
 				><p class="nav-text">Contact</p></a
@@ -66,7 +83,7 @@
 		<button
 			data-aos="flip-up"
 			data-aos-delay="600"
-      data-aos-duration="700"
+			data-aos-duration="700"
 			on:click={toggleTheme}
 			class="border-gray-500 border-2 py-1 px-3 dark:text-gray-50 rounded-md"
 			><i class="{dark ? 'fa fa-sun' : 'far fa-moon'} mr-2" />{dark ? "Light" : "Dark"}</button
